27a2b0c8cb5dd333bc5c845b7c96bebb55c2269f
Xinhe Huang <xinhe.huang@stonybrook.edu>
2016-02-06 18:30:13 -0500

    add mouseover

--- myapp.js
+++ myapp.js
@@ -4,7 +4,7 @@
 
 var w = 400;
 var h = 100;
-var barPadding = 1;
+var barPadding = 10;
 
 var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
     11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
@@ -36,13 +36,13 @@ svg.selectAll("rect")
     .attr("height", function (d) {
         return d * 4;
     })
-    .attr("fill", "gray")
     .on('mouseover', function (d,i) {
         $(this).attr("x",
-            i * (w / dataset.length)-5);
-        $(this).attr("width", w / dataset.length - barPadding + 10);
+            i * (w / dataset.length)-2.5);
+        $(this).attr("width", w / dataset.length - barPadding + 5);
         $(this).attr("y", h - d * 4 - 10);
         $(this).attr("height", d * 4 + 10);
+        $(this).css("opacity",1);
         tip.show(d)
     })
     .on('mouseout', function (d,i) {
@@ -51,7 +51,7 @@ svg.selectAll("rect")
         $(this).attr("width", w / dataset.length - barPadding);
         $(this).attr("y", h - d * 4);
         $(this).attr("height", d * 4);
-
+        $(this).css("opacity",0.6);
         tip.hide(d);
     });
 
